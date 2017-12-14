7eb669602dd08278960b55ccf946ee9737d7cb33
Mirza Sisic <mirzasisic57@gmail.com>
2017-12-14 16:54:03 -0500

    Header & Background
    Change header content and background image.

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 <?php include("includes/mobile.php"); ?>
 <!-- Header––––––––––––– -->
 <?php include("includes/header.php"); ?>
-
   <!-- MASTER CONTENT
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <section class="mastwrap">
@@ -17,26 +16,24 @@
 
 <section class="inner-page-wrap">
 
-
+<!------>
 
 <div class="container-fluid intro11-bg">
     <div class="row">
         <article class="col-md-8 col-md-offset-2 common-content fullheight text-center">
             <div class="valign">
-                <img data-no-retina alt="" title="Bururet" class="img-responsive" src="images/bururet.jpg" style="margin-top: 1cm;"/>
-                <h2 class="font4bold dark main-caps-big">SafetBeg</h2>
-                <p class="add-top-quarter">Fine Arts and Photography</p>
+                <img data-no-retina/>
             </div>
         </article>
     </div>
 </div>
 
-
-
+<div class="text-center">
+  <h3>Fine Arts and Photography</h3>
+</div>
 
 </section>
   <!-- end : inner page wrap -->
-
 
  </section>
   <!-- end : innerwrap -->

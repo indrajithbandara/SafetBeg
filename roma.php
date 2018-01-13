d2c347d488885dd5d01b6b1d30eb963e974c4cb4
Mirza Sisic <mirzasisic57@gmail.com>
2018-01-12 20:26:36 -0500

    Test

--- roma.php
+++ roma.php
@@ -65,7 +65,7 @@
 
     <!-- start : works-item -->
     <div class="works-item info ui logos">
-            <img data-no-retina alt="Roma Child"  class="img-responsive" src="images/romiMalac.png"/>
+            <img data-no-retina alt="Roma Child"  class="img-responsive" src="images/RomiMalac.png"/>
     </div>
     <!-- end : works-item -->
 
